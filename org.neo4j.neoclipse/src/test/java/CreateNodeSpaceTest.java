163586daae30e04fc3eb5610a8648217bc1d2983
Anders Nawroth <anders@neotechnology.com>
2008-06-11 07:59:26 +0000

    Added node types, renamed class.
    git-svn-id: https://svn.neo4j.org/components/neoclipse/trunk@1797 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- org.neo4j.neoclipse/src/test/java/CreateNodeSpaceTest.java
+++ org.neo4j.neoclipse/src/test/java/CreateNodeSpaceTest.java
@@ -7,9 +7,10 @@ import org.neo4j.api.core.NeoService;
 import org.neo4j.api.core.Node;
 import org.neo4j.api.core.Transaction;
 
-public class CeateNodeSpaceTest{
+public class CreateNodeSpaceTest{
 
 	private static final String NAME = "NAME";
+	private static final String NODE_TYPE = "NODE_TYPE";
 	
 	@Test
 	public void testCreateSimpleNodeSpace() {
@@ -18,16 +19,21 @@ public class CeateNodeSpaceTest{
 		try {
 			Node referenceNode = neo.getReferenceNode();
 			referenceNode.setProperty(NAME, "referenceNode");
+			referenceNode.setProperty( NODE_TYPE, "referenceNode" );
 			Node peter = neo.createNode();
 			peter.setProperty(NAME, "Peter");
+			peter.setProperty( NODE_TYPE, "human" );
 			Node li = neo.createNode();
 			li.setProperty(NAME, "Li");
+            li.setProperty( NODE_TYPE, "human" );
 
 			Node blaff = neo.createNode();
 			blaff.setProperty(NAME, "blaff");
+            blaff.setProperty( NODE_TYPE, "animal" );
 
 			Node woff = neo.createNode();
 			woff.setProperty(NAME, "woff");
+            woff.setProperty( NODE_TYPE, "animal" );
 			
 			referenceNode.createRelationshipTo(peter,
 					MyRels.ROOT);
