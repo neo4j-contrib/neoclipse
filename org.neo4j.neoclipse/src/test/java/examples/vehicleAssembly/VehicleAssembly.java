0a2145c6784948a37d510dedddf287ddf2038da2
Anders Nawroth <anders@neotechnology.com>
2008-06-18 16:40:32 +0000

    Some refactoring.
    git-svn-id: https://svn.neo4j.org/components/neoclipse/trunk@1874 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- org.neo4j.neoclipse/src/test/java/examples/vehicleAssembly/VehicleAssembly.java
+++ org.neo4j.neoclipse/src/test/java/examples/vehicleAssembly/VehicleAssembly.java
@@ -13,6 +13,7 @@ import examples.NeoclipseExample;
 
 public class VehicleAssembly extends NeoclipseExample
 {
+    private static final String NT_REFERENCE = "referenceNode";
     private static final String NT_VEHICLE = "vehicle";
     private static final String NT_PART = "part";
     private static final String NAME = "NAME";
@@ -28,66 +29,38 @@ public class VehicleAssembly extends NeoclipseExample
         {
             Node referenceNode = neo.getReferenceNode();
             referenceNode.setProperty( NAME, "referenceNode" );
-            referenceNode.setProperty( NODE_TYPE, "referenceNode" );
+            referenceNode.setProperty( NODE_TYPE, NT_REFERENCE );
             Node trike = neo.createNode();
             trike.setProperty( NAME, "trike" );
             trike.setProperty( NODE_TYPE, NT_VEHICLE );
             trike.setProperty( COST, 3 );
             referenceNode.createRelationshipTo( trike, VehicleRels.VEHICLE );
-            Node wheel = neo.createNode();
-            wheel.setProperty( NAME, "wheel" );
-            wheel.setProperty( NODE_TYPE, NT_PART );
-            wheel.setProperty( COST, 3 );
-            trike.createRelationshipTo( wheel, VehicleRels.CONTAINS )
-                .setProperty( QUANTITY, 3 );
-            Node frame = neo.createNode();
-            frame.setProperty( NAME, "frame" );
-            frame.setProperty( NODE_TYPE, NT_PART );
-            frame.setProperty( COST, 15 );
-            trike.createRelationshipTo( frame, VehicleRels.CONTAINS )
-                .setProperty( QUANTITY, 1 );
-            Node spoke = neo.createNode();
-            spoke.setProperty( NAME, "spoke" );
-            spoke.setProperty( NODE_TYPE, NT_PART );
-            spoke.setProperty( COST, 1 );
-            wheel.createRelationshipTo( spoke, VehicleRels.CONTAINS )
-                .setProperty( QUANTITY, 2 );
-            Node tire = neo.createNode();
-            tire.setProperty( NAME, "tire" );
-            tire.setProperty( NODE_TYPE, NT_PART );
-            tire.setProperty( COST, 2 );
-            wheel.createRelationshipTo( tire, VehicleRels.CONTAINS )
-                .setProperty( QUANTITY, 1 );
-            Node rim = neo.createNode();
-            rim.setProperty( NAME, "rim" );
-            rim.setProperty( NODE_TYPE, NT_PART );
-            rim.setProperty( COST, 2 );
-            tire.createRelationshipTo( rim, VehicleRels.CONTAINS ).setProperty(
-                QUANTITY, 1 );
-            Node tube = neo.createNode();
-            tube.setProperty( NAME, "tube" );
-            tube.setProperty( NODE_TYPE, NT_PART );
-            tube.setProperty( COST, 1 );
-            tire.createRelationshipTo( tube, VehicleRels.CONTAINS )
-                .setProperty( QUANTITY, 1 );
-            Node seat = neo.createNode();
-            seat.setProperty( NAME, "seat" );
-            seat.setProperty( NODE_TYPE, NT_PART );
-            seat.setProperty( COST, 4 );
-            frame.createRelationshipTo( seat, VehicleRels.CONTAINS )
-                .setProperty( QUANTITY, 1 );
-            Node pedal = neo.createNode();
-            pedal.setProperty( NAME, "pedal" );
-            pedal.setProperty( NODE_TYPE, NT_PART );
-            pedal.setProperty( COST, 1 );
-            frame.createRelationshipTo( pedal, VehicleRels.CONTAINS )
-                .setProperty( QUANTITY, 1 );
+            Node wheel = createPart( "wheel", 3, trike, 3 );
+            Node frame = createPart( "frame", 15, trike, 1 );
+            createPart( "spoke", 1, wheel, 2 );
+            Node tire = createPart( "tire", 2, wheel, 1 );
+            createPart( "rim", 2, tire, 1 );
+            createPart( "tube", 1, tire, 1 );
+            createPart( "seat", 4, frame, 1 );
+            createPart( "pedal", 1, frame, 1 );
             tx.success();
         }
         finally
         {
             tx.finish();
         }
+    }
+
+    private Node createPart( String name, int cost, Node containedIn,
+        int quantity )
+    {
+        Node part = neo.createNode();
+        part.setProperty( NAME, name );
+        part.setProperty( NODE_TYPE, NT_PART );
+        part.setProperty( COST, cost );
+        containedIn.createRelationshipTo( part, VehicleRels.CONTAINS )
+            .setProperty( QUANTITY, quantity );
+        return part;
     }
 
     @Test
@@ -105,16 +78,16 @@ public class VehicleAssembly extends NeoclipseExample
                     Traverser.Order.DEPTH_FIRST, StopEvaluator.END_OF_NETWORK,
                     ReturnableEvaluator.ALL_BUT_START_NODE,
                     VehicleRels.CONTAINS, Direction.OUTGOING );
-                for ( Node node : traverser )
+                for ( Node part : traverser )
                 {
                     int depth = traverser.currentPosition().depth();
-                    Relationship rel = traverser.currentPosition()
-                        .lastRelationshipTraversed();
                     for ( int i = 0; i < depth; i++ )
                     {
                         System.out.print( "  " );
                     }
-                    System.out.print( node.getProperty( NAME ) );
+                    System.out.print( part.getProperty( NAME ) );
+                    Relationship rel = traverser.currentPosition()
+                        .lastRelationshipTraversed();
                     if ( rel != null )
                     {
                         System.out.print( " " + rel.getProperty( QUANTITY, 0 ) );
